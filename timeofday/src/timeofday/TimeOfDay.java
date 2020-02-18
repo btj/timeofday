18d7a1ec4f7ed5399378c72168f9ac2ebe188af8
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2020-02-18 08:48:53 +0100

    Implicit this

--- timeofday/src/timeofday/TimeOfDay.java
+++ timeofday/src/timeofday/TimeOfDay.java
@@ -4,20 +4,20 @@ class TimeOfDay {
 	int minutesSinceMidnight;
 
 	int getHours() {
-		return this.minutesSinceMidnight / 60;
+		return minutesSinceMidnight / 60;
 	}
 
 	int getMinutes() {
-		return this.minutesSinceMidnight % 60;
+		return minutesSinceMidnight % 60;
 	}
 
 	void setHours(int value) {
-		this.minutesSinceMidnight %= 60;
-		this.minutesSinceMidnight += value * 60;
+		minutesSinceMidnight %= 60;
+		minutesSinceMidnight += value * 60;
 	}
 
 	void setMinutes(int value) {
-		this.minutesSinceMidnight = this.minutesSinceMidnight / 60 * 60 + value;
+		minutesSinceMidnight = minutesSinceMidnight / 60 * 60 + value;
 	}
 
 }
